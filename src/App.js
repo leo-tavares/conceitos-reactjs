f0237edc9bde4c88e17a508e216ff5aaec472f19
leo-tavares <leonardotavaresdev@gmail.com>
2020-05-30 18:07:42 -0300

    feat: List all repositories

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [repositories, setRepositores] = useState([]);
+  useEffect(() => {
+    const fetchRepositories = async () => {
+      const response = await api.get("repositories");
+      setRepositores(response.data);
+    };
+    fetchRepositories();
+  }, []);
+
   async function handleAddRepository() {
     // TODO
   }
@@ -14,13 +24,12 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repo) => (
+          <li>
+            {repo.title}
+            <button onClick={() => handleRemoveRepository(1)}>Remover</button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
